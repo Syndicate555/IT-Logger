e1ceb9b268a64e3e89541ef31722677f5c810165
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 15:36:18 -0400

    Update App.js

--- itlogger/src/App.js
+++ itlogger/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
