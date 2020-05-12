9cfd3eb4202323e17e52eee1131ee3784a16464d
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 19:38:35 -0400

    changes

--- itlogger/src/App.js
+++ itlogger/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
-import Logs from "./components/logs/LogItem";
+import Logs from "./components/logs/Logs";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
