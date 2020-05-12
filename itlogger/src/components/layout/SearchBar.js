0f239e6c0bd146bf70be1859684eff676406ea03
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 19:04:04 -0400

    Update SearchBar.js

--- itlogger/src/components/layout/SearchBar.js
+++ itlogger/src/components/layout/SearchBar.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const SearchBar = () => {
-  return <div></div>;
+  return  <nav style={{ marginBottom: '30px' }} className='blue'>
+  <div className='nav-wrapper'>
+    <form>
+      <div className='input-field'>
+        <input
+          id='search'
+          type='search'
+          placeholder='Search Logs..'
+          ref={text}
+          onChange={onChange}
+        />
+        <label className='label-icon' htmlFor='search'>
+          <i className='material-icons'>search</i>
+        </label>
+        <i className='material-icons'>close</i>
+      </div>
+    </form>
+  </div>
+</nav>
+);
+};
 };
 
 export default SearchBar;
