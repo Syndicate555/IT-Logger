6a3763b03a4e2ac37aab4ab5ca0a642c71bf10fd
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 19:07:20 -0400

    changes

--- itlogger/src/App.js
+++ itlogger/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import SearchBar from "./components/layout/SearchBar";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
--- itlogger/src/components/layout/SearchBar.js
+++ itlogger/src/components/layout/SearchBar.js
@@ -1,26 +1,20 @@
 import React from "react";
 
 const SearchBar = () => {
-    return (
-        <nav style={{ marginBottom: '30px' }} className='blue'>
-          <div className='nav-wrapper'>
-            <form>
-              <div className='input-field'>
-                <input
-                  id='search'
-                  type='search'
-                  placeholder='Search Logs..'
-                  ref={text}
-                  onChange={onChange}
-                />
-                <label className='label-icon' htmlFor='search'>
-                  <i className='material-icons'>search</i>
-                </label>
-                <i className='material-icons'>close</i>
-              </div>
-            </form>
+  return (
+    <nav style={{ marginBottom: "30px" }} className="blue">
+      <div className="nav-wrapper">
+        <form>
+          <div className="input-field">
+            <input id="search" type="search" />
+            <label className="label-icon" htmlFor="search">
+              <i className="material-icons">search</i>
+            </label>
+            <i className="material-icons">close</i>
           </div>
-        </nav>
-      );
-
+        </form>
+      </div>
+    </nav>
+  );
+};
 export default SearchBar;
