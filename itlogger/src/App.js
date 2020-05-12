dc359577862779e4d24aa72e0d4564d4ff3b3d1f
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 15:35:50 -0400

    Update App.js

--- itlogger/src/App.js
+++ itlogger/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
-
+import M from "materialize-css/dist/js/materialize.min.js";
 const App = () => {
   return <div className="App">My App</div>;
 };
