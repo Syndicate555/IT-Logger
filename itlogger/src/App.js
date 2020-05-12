356bc0cef12612f0240583d38b100e86af8adda9
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 15:37:33 -0400

    Update App.js

--- itlogger/src/App.js
+++ itlogger/src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+  });
   return <div className="App">My App</div>;
 };
 
