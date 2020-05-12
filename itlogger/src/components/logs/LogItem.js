db40d058b477af0fd0905fdbfbadf58a827b639d
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 19:13:35 -0400

    Update LogItem.js

--- itlogger/src/components/logs/LogItem.js
+++ itlogger/src/components/logs/LogItem.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export const LogItem = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
   return <div></div>;
 };
