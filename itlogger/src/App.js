221f57a5f764ab6659c8f6f1cc8af99c66a58e0f
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 19:31:48 -0400

    Update App.js

--- itlogger/src/App.js
+++ itlogger/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
-import LogItem from "./components/logs/LogItem";
+import Logs from "./components/logs/LogItem";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -12,6 +12,9 @@ const App = () => {
   return (
     <Fragment>
       <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
     </Fragment>
   );
 };
