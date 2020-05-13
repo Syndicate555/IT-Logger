6ec61b6adca64d4bf30d2c0ad45fe37be0712eb1
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 01:58:05 -0400

    Update AddLogModal.js

--- itlogger/src/components/logs/AddLogModal.js
+++ itlogger/src/components/logs/AddLogModal.js
@@ -3,5 +3,6 @@ import React, { udeState } from "react";
 export const AddLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState("");
   return <div></div>;
 };
