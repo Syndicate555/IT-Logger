0d0a7461a2f93725e6e134eeb310a15d432f0477
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 12:17:55 -0400

    Update AddLogModal.js

--- itlogger/src/components/logs/AddLogModal.js
+++ itlogger/src/components/logs/AddLogModal.js
@@ -4,7 +4,7 @@ const AddLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
-  return <div id="add-log-modal" className="modal"></div>;
+  return <div id="add-log-modal" className="modal" style={modalStyle}></div>;
 };
 
 export default AddLogModal;
