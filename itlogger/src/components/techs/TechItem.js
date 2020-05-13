8aee13d10f41a63ce6b33069f6834b29ec55f028
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 19:34:57 -0400

    Update TechItem.js

--- itlogger/src/components/techs/TechItem.js
+++ itlogger/src/components/techs/TechItem.js
@@ -1,5 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export const TechItem = () => {
+const TechItem = (props) => {
   return <div></div>;
 };
+
+TechItem.propTypes = {};
+
+export default TechItem;
