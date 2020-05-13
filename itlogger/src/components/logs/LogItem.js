e11f0dd6b223e7f451970a3521fe4c9b34195a9b
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 21:11:26 -0400

    Update LogItem.js

--- itlogger/src/components/logs/LogItem.js
+++ itlogger/src/components/logs/LogItem.js
@@ -2,7 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const LogItem = ({ log }) => {
-  return <div></div>;
+  return (
+    <li>
+      <div>
+        <a href="">{log.message}</a>
+      </div>
+    </li>
+  );
 };
 
 LogItem.propTypes = { log: PropTypes.object.isRequired };
