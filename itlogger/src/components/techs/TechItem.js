f4644a4097ff7fe9353850cf898e78600bd87001
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 19:38:09 -0400

    Update TechItem.js

--- itlogger/src/components/techs/TechItem.js
+++ itlogger/src/components/techs/TechItem.js
@@ -6,6 +6,9 @@ const TechItem = (tech) => {
     <li className="collection-item">
       <div>
         {tech.firstName} {tech.lastName}
+        <a href="#!" className="secondary-content">
+          <i className="material-icons grey-text">delete</i>
+        </a>
       </div>
     </li>
   );
