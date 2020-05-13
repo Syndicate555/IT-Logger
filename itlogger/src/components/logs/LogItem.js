197d4bd8318e4202117a1e90cd0ffa8372892aa5
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 21:09:08 -0400

    Update LogItem.js

--- itlogger/src/components/logs/LogItem.js
+++ itlogger/src/components/logs/LogItem.js
@@ -1,37 +1,10 @@
-import React, { useState, useEffect } from "react";
-const LogItem = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
+import React from "react";
+import PropTypes from "prop-types";
 
-  useEffect(() => {
-    getLogs();
-    //eslint-disable-next-line
-  }, []);
-
-  const getLogs = async () => {
-    setLoading(true);
-    const res = await fetch("/logs");
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
-    return <h4>Loading.........</h4>;
-  }
-  return (
-    <ul className="collection-with-header">
-      <li className="collection-header">
-        <h4 className="center">System Logs</h4>
-      </li>
-      {!loading && logs.length === 0 ? (
-        <p className="center">No logs to show.....</p>
-      ) : (
-        logs.map((log) => <li>{log.message}</li>)
-      )}
-    </ul>
-  );
+const LogItem = (props) => {
+  return <div></div>;
 };
+
+LogItem.propTypes = {};
 
 export default LogItem;
