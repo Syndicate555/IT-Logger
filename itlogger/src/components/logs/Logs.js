43aa086d7d4572a0fdf56061bb3cfa940f48df7d
Syndicate555 <saziz028@uottawa.ca>
2020-05-12 21:08:35 -0400

    Update Logs.js

--- itlogger/src/components/logs/Logs.js
+++ itlogger/src/components/logs/Logs.js
@@ -21,7 +21,7 @@ const Logs = () => {
     return <h4>Loading.........</h4>;
   }
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">System Logs</h4>
       </li>
