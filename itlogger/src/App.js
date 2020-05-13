433fd5001096ff3c35cca2495f03a165cc951c1e
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 01:01:05 -0400

    Update App.js

--- itlogger/src/App.js
+++ itlogger/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
+import AddBtn from "./components/layout/AddBtn";
 import Logs from "./components/logs/Logs";
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
