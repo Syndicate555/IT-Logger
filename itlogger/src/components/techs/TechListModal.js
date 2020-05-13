95ef1941008967c456eb128dfe4c92ae0f367441
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 19:17:41 -0400

    Update TechListModal.js

--- itlogger/src/components/techs/TechListModal.js
+++ itlogger/src/components/techs/TechListModal.js
@@ -22,19 +22,7 @@ const TechListModal = () => {
     return <Preloader />;
   }
   return (
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4 className="center" style={{ fontFamily: "Jura" }}>
-          {" "}
-          System Logs
-        </h4>
-      </li>
-      {!loading && logs.length === 0 ? (
-        <p className="center">No logs to show.....</p>
-      ) : (
-        logs.map((log) => <LogItem log={log} key={log.id} />)
-      )}
-    </ul>
+    
   );
 };
 
