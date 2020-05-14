23b74b89013624609f3050341242f1b64b003528
Syndicate555 <saziz028@uottawa.ca>
2020-05-14 18:21:17 -0400

    Update Logs.js

--- itlogger/src/components/logs/Logs.js
+++ itlogger/src/components/logs/Logs.js
@@ -1,27 +1,27 @@
 import React, { useEffect } from "react";
-import LogItem from "./LogItem";
 import { connect } from "react-redux";
+import LogItem from "./LogItem";
+import Preloader from "../layout/Preloader";
 import PropTypes from "prop-types";
 import { getLogs } from "../../actions/logActions";
-import Preloader from "../layout/Preloader";
+
 const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
-    //eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
 
   if (loading || logs === null) {
     return <Preloader />;
   }
+
   return (
     <ul className="collection with-header">
       <li className="collection-header">
-        <h4 className="center" style={{ fontFamily: "Jura" }}>
-          System Logs
-        </h4>
+        <h4 className="center">System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
-        <p className="center">No logs to show.....</p>
+        <p className="center">No logs to show...</p>
       ) : (
         logs.map((log) => <LogItem log={log} key={log.id} />)
       )}
@@ -31,8 +31,11 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
 
 Logs.propTypes = {
   log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired,
 };
+
 const mapStateToProps = (state) => ({
   log: state.log,
 });
+
 export default connect(mapStateToProps, { getLogs })(Logs);
