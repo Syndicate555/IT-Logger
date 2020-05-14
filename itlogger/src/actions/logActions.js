7a1aabb1329a97336a4d29fac81d442df31964e3
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 21:30:02 -0400

    Update logActions.js

--- itlogger/src/actions/logActions.js
+++ itlogger/src/actions/logActions.js
@@ -13,6 +13,20 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 //   };
 // };
 
+export const getLogs = () => {
+  return async (dispatch) => {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  };
+};
+
 //set loading to true
 export const setLoading = () => {
   return {
