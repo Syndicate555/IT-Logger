937a2fc9dfd13ce7c9c237084eef7012649939b8
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 21:31:29 -0400

    Update logActions.js

--- itlogger/src/actions/logActions.js
+++ itlogger/src/actions/logActions.js
@@ -15,6 +15,19 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
 export const getLogs = () => async (dispatch) => {
   try {
+    export const getLogs = () => {
+      return async (dispatch) => {
+        setLoading();
+
+        const res = await fetch("/logs");
+        const data = await res.json();
+
+        dispatch({
+          type: GET_LOGS,
+          payload: data,
+        });
+      };
+    };
   } catch (error) {}
   setLoading();
 
