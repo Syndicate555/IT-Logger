1dda59c7380a56afeaacdb87714d2df0c719d219
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 20:54:30 -0400

    Update store.js

--- itlogger/src/store.js
+++ itlogger/src/store.js
@@ -1,2 +1,4 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
