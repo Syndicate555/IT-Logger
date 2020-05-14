b815f4e4fe7fd813a71b8af2786b30bb9a652a55
Syndicate555 <saziz028@uottawa.ca>
2020-05-14 18:47:05 -0400

    Update Logs.js

--- itlogger/src/components/logs/Logs.js
+++ itlogger/src/components/logs/Logs.js
@@ -18,7 +18,12 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
   return (
     <ul className="collection with-header">
       <li className="collection-header">
-        <h4 className="center">System Logs</h4>
+        <h4
+          className="center"
+          style={{ fontFamily: "Jura", textAlign: "center" }}
+        >
+          System Logs
+        </h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className="center">No logs to show...</p>
