12b8817e77fb546d8839080191da4452ce318c54
Syndicate555 <saziz028@uottawa.ca>
2020-05-14 18:13:04 -0400

    Update Logs.js

--- itlogger/src/components/logs/Logs.js
+++ itlogger/src/components/logs/Logs.js
@@ -11,7 +11,7 @@ const Logs = ({ log: { logs, loading } }) => {
     //eslint-disable-next-line
   }, []);
 
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
   return (
