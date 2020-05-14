bdaee893f5f2d1fe57ef9512d6c6af4dadfc92ab
Syndicate555 <saziz028@uottawa.ca>
2020-05-14 18:06:54 -0400

    Update Logs.js

--- itlogger/src/components/logs/Logs.js
+++ itlogger/src/components/logs/Logs.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import LogItem from "./LogItem";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
