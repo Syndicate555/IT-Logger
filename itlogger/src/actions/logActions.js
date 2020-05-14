fca371481dbf01bda465d2b8312b4f2b9d4f8436
Syndicate555 <saziz028@uottawa.ca>
2020-05-13 21:24:40 -0400

    Update logActions.js

--- itlogger/src/actions/logActions.js
+++ itlogger/src/actions/logActions.js
@@ -1,6 +1,7 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 export const getLogs = () => {};
 
+//set loading to true
 export const setLoading = () => {
   return {
     type: SET_LOADING,
