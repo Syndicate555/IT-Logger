db668bb32b4abe494449d6a6b1667f29e5ffef51
Syndicate555 <saziz028@uottawa.ca>
2020-05-14 22:07:51 -0400

    Update logReducer.js

--- itlogger/src/reducers/logReducer.js
+++ itlogger/src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "../actions/types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "../actions/types";
 
 //App Level State
 const initalState = {
