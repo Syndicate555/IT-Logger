b69aedc92c221095535f50f32d38543a4162fad1
Syndicate555 <saziz028@uottawa.ca>
2020-05-15 15:32:06 -0400

    Update LogItem.js

--- itlogger/src/components/logs/LogItem.js
+++ itlogger/src/components/logs/LogItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Moment from "react-moment";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 const LogItem = ({ log }) => {
   return (
