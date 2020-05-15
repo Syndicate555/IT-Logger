f6f97f55a13d2d1a069da4ed506f29aaf87c8ef9
Syndicate555 <saziz028@uottawa.ca>
2020-05-15 15:23:54 -0400

    Update logActions.js

--- itlogger/src/actions/logActions.js
+++ itlogger/src/actions/logActions.js
@@ -61,7 +61,7 @@ export const addLog = (log) => async (dispatch) => {
 
 //Delete log from server
 
-export const getLogs = () => async (dispatch) => {
+export const deleteLogs = () => async (dispatch) => {
   try {
     setLoading();
 
