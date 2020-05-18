b88eedea964fc6e36aa134ade36d8b987cc2040a
Syndicate555 <saziz028@uottawa.ca>
2020-05-18 19:11:03 -0400

    Update LogItem.js

--- itlogger/src/components/logs/LogItem.js
+++ itlogger/src/components/logs/LogItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import Moment from "react-moment";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { deleteLog } from "../../actions/logActions";
+import { deleteLog, setCurrent } from "../../actions/logActions";
 import M from "materialize-css/dist/js/materialize.min.js";
 
 const LogItem = ({ log, deleteLog }) => {
