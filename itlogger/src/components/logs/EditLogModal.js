acdbf56e111e0f805677eae0a14c88e396bee521
Syndicate555 <saziz028@uottawa.ca>
2020-05-18 19:22:50 -0400

    database changes

--- itlogger/src/components/logs/EditLogModal.js
+++ itlogger/src/components/logs/EditLogModal.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
 import M from "materialize-css/dist/js/materialize.min.js";
 
 const EditLogModal = () => {
