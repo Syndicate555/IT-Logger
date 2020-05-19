3df6c79d3514d647294a1cb77974d824924bde3c
Syndicate555 <saziz028@uottawa.ca>
2020-05-18 21:57:48 -0400

    Update SearchBar.js

--- itlogger/src/components/layout/SearchBar.js
+++ itlogger/src/components/layout/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { searchLogs } from "../../actions/logActions";
