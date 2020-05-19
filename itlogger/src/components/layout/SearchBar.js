d40cf3f924d39d06b04efc3ba39318e0d7d33972
Syndicate555 <saziz028@uottawa.ca>
2020-05-18 22:00:04 -0400

    Update SearchBar.js

--- itlogger/src/components/layout/SearchBar.js
+++ itlogger/src/components/layout/SearchBar.js
@@ -5,12 +5,21 @@ import { searchLogs } from "../../actions/logActions";
 
 const SearchBar = ({ searchLogs }) => {
   const text = useRef("");
+  const onChange(() => {
+    
+  })
   return (
     <nav style={{ marginBottom: "30px" }} className="blue">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" placeholder="Search Logs......" />
+            <input
+              id="search"
+              type="search"
+              placeholder="Search Logs......"
+              ref={text}
+              onChange={onChange}
+            />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
