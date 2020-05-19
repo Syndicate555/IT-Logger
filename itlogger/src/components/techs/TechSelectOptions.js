5f88af1551ca078ed8dbcf5676174bdc8e388801
Syndicate555 <saziz028@uottawa.ca>
2020-05-19 05:19:08 -0400

    Update TechSelectOptions.js

--- itlogger/src/components/techs/TechSelectOptions.js
+++ itlogger/src/components/techs/TechSelectOptions.js
@@ -7,6 +7,13 @@ const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
   return <div></div>;
 };
 
-TechSelectOptions.propTypes = {};
+TechSelectOptions.propTypes = {
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  tech: state.tech,
+});
 
 export default connect(mapStateToProps, { getTechs })(TechSelectOptions);
