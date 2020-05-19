ef0a6033e0b61823cab92361316d721142e324a5
Syndicate555 <saziz028@uottawa.ca>
2020-05-19 08:33:07 -0400

    Update TechSelectOptions.js

--- itlogger/src/components/techs/TechSelectOptions.js
+++ itlogger/src/components/techs/TechSelectOptions.js
@@ -1,19 +1,20 @@
 import React, { useEffect } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { getTechs } from "../../actions/techActions";
 
 const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
   useEffect(() => {
     getTechs();
-    //eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
+
   return (
     !loading &&
     techs !== null &&
     techs.map((t) => (
-      <option key={t.id} value={`${t.firstName}  ${t.lastName}`}>
-        {t.firstName} {t.firstName}
+      <option key={t.id} value={`${t.firstName} ${t.lastName}`}>
+        {t.firstName} {t.lastName}
       </option>
     ))
   );
