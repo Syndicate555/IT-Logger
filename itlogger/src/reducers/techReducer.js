0b6beffbdeabc35fa33efca94ea203ed6bb21010
Syndicate555 <saziz028@uottawa.ca>
2020-05-19 03:09:15 -0400

    Update techReducer.js

--- itlogger/src/reducers/techReducer.js
+++ itlogger/src/reducers/techReducer.js
@@ -14,6 +14,12 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false,
+      };
     case SET_LOADING:
       return {
         ...state,
