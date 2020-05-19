b7aac6f795e04b8cc0b0f1e555b76a017c9c6e38
Syndicate555 <saziz028@uottawa.ca>
2020-05-18 21:32:12 -0400

    search logs added

--- itlogger/src/actions/logActions.js
+++ itlogger/src/actions/logActions.js
@@ -117,6 +117,26 @@ export const updateLog = (log) => async (dispatch) => {
   }
 };
 
+// Search logs
+export const searchLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
 // Set Current Log
 
 export const setCurrent = (log) => {
