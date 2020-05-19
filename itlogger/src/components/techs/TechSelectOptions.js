b259a3ccf9889b47bae0864344d145949dc337c2
Syndicate555 <saziz028@uottawa.ca>
2020-05-19 04:40:32 -0400

    Update TechSelectOptions.js

--- itlogger/src/components/techs/TechSelectOptions.js
+++ itlogger/src/components/techs/TechSelectOptions.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getTechs } from "../../actions/techActions";
 
 const TechSelectOptions = (props) => {
   return <div></div>;
