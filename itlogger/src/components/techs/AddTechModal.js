287e6e2f1f75e69d1f000166bcc98684abc941b0
Syndicate555 <saziz028@uottawa.ca>
2020-05-19 07:27:33 -0400

    Update AddTechModal.js

--- itlogger/src/components/techs/AddTechModal.js
+++ itlogger/src/components/techs/AddTechModal.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addTech } from "../../actions/techActions";
 import M from "materialize-css/dist/js/materialize.min.js";
 
 const AddTechModal = () => {
