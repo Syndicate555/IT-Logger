8aabff5b706fe3ad6dc88be76af79529760403c8
Syndicate555 <saziz028@uottawa.ca>
2020-05-19 03:00:30 -0400

    Update techActions.js

--- itlogger/src/actions/techActions.js
+++ itlogger/src/actions/techActions.js
@@ -7,6 +7,24 @@ import {
 } from "./types";
 
 // Get techs from server
+export const getTechs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/techs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
 
 // Set loading to true
 export const setLoading = () => {
