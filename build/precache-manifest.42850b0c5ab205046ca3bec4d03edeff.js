421c70ccd9ab355b099ee4004710299dd3bb3e87
Syndicate555 <saziz028@uottawa.ca>
2020-07-08 09:08:49 -0400

    static bundle

---
+++ build/precache-manifest.42850b0c5ab205046ca3bec4d03edeff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751f993b47246ce5903f5a9ea53aa276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf24215bcb4db94ff2e0",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "b8e5826e2c05bf232a6f",
+    "url": "/static/css/main.e2580086.chunk.css"
+  },
+  {
+    "revision": "bf24215bcb4db94ff2e0",
+    "url": "/static/js/2.7e0c9832.chunk.js"
+  },
+  {
+    "revision": "b86edb0b1abd3f3b99a167e3673506ee",
+    "url": "/static/js/2.7e0c9832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e5826e2c05bf232a6f",
+    "url": "/static/js/main.6dc00fb6.chunk.js"
+  },
+  {
+    "revision": "cd31910df262b0adc69b",
+    "url": "/static/js/runtime-main.e2e2d47d.js"
+  },
+  {
+    "revision": "04653daa2aea957db855efb4bf79d05e",
+    "url": "/static/media/default.04653daa.png"
+  },
+  {
+    "revision": "29bccc1d065048263be35c196cb65938",
+    "url": "/static/media/default1.29bccc1d.png"
+  }
+]);
