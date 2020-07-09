7ff263f419789c59c1c3c0f53b0038bd6b3d6935
Syndicate555 <saziz028@uottawa.ca>
2020-07-09 07:43:02 -0400

    Update logActions.js

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -10,20 +10,6 @@ import {
   CLEAR_CURRENT,
 } from "./types";
 
-// export const getLogs = () => {
-//   return async dispatch => {
-//     setLoading();
-
-//     const res = await fetch('/logs');
-//     const data = await res.json();
-
-//     dispatch({
-//       type: GET_LOGS,
-//       payload: data
-//     });
-//   };
-// };
-
 // Get logs from server
 export const getLogs = () => async (dispatch) => {
   try {
